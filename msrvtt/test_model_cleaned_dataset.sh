1ad7ac83a93a6118cb7d4629ef058c24431150cc
Chen Haoran <chenhaoran.earth@live.com>
2020-08-13 15:12:03 +0800

    third update

--- msrvtt/test_model_cleaned_dataset.sh
+++ msrvtt/test_model_cleaned_dataset.sh
@@ -3,8 +3,8 @@ TF_XLA_FLAGS=--tf_xla_cpu_global_jit \
 XLA_FLAGS=--xla_hlo_profile \
 CUDA_VISIBLE_DEVICES=8 \
 python train_model.py --name $name \
-    --corpus ../../msrvtt_dataset/msrvtt_corpus_cleaned_v2.pkl \
-    --ecores ../../msrvtt_dataset/msrvtt_eco_res_avg_norm.npy \
-    --tag    ../../msrvtt_dataset/msrvtt_semantic_tag_eco_res_avg_cleaned_v2.npy \
-    --ref    ../../msrvtt_dataset/msrvtt_ref3.pkl \
-    --test   ./saves2/msrvtt_ref-best.ckpt
+    --corpus ../../msrvtt_feats/msrvtt_corpus_cleaned.pkl \
+    --ecores ../../msrvtt_feats/msrvtt_eco_res_avg_norm.npy \
+    --tag    ../../msrvtt_feats/msrvtt_semantic_tag_eco_res_avg_cleaned.npy \
+    --ref    ../../msrvtt_feats/msrvtt_ref_cleaned.pkl \
+    --test   ./saves/msrvtt_ref-best.ckpt
--- msrvtt/test_model_cleaned_dataset_without_duplicate_removal.sh
+++ msrvtt/test_model_cleaned_dataset_without_duplicate_removal.sh
@@ -1,11 +1,10 @@
-name=msrvtt_ref_cleaned_ref_complete
+name=msrvtt_ref_cleaned_without_duplicate_removal
 TF_XLA_FLAGS=--tf_xla_cpu_global_jit \
 XLA_FLAGS=--xla_hlo_profile \
 CUDA_VISIBLE_DEVICES=8 \
 python train_model.py --name $name \
-    --corpus ../../msrvtt_dataset/msrvtt_corpus_cleaned_v2.pkl \
-    --ecores ../../msrvtt_dataset/msrvtt_eco_res_avg_norm.npy \
-    --tag    ../../msrvtt_dataset/msrvtt_semantic_tag_eco_res_avg_cleaned_v2.npy \
-    --ref    ../../msrvtt_dataset/msrvtt_ref_v3_complete.pkl \
-    --test   ./saves2/msrvtt_ref-best.ckpt
-    > ./log2/${name}.log
+    --corpus ../../msrvtt_feats/msrvtt_corpus_cleaned.pkl \
+    --ecores ../../msrvtt_feats/msrvtt_eco_res_avg_norm.npy \
+    --tag    ../../msrvtt_feats/msrvtt_semantic_tag_eco_res_avg_cleaned.npy \
+    --ref    ../../msrvtt_feats/msrvtt_ref_cleaned_without_duplicate_removal.pkl \
+    --test   ./saves/msrvtt_ref-best.ckpt
